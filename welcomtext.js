ee372b406e3bcd230dc89b6f46ac9e55a8583326
Mauricio504 <81764805+Mauricio504@users.noreply.github.com>
2021-04-16 12:02:51 -0600

    Update welcomtext.js

--- welcomtext.js
+++ welcomtext.js
@@ -1,9 +1,9 @@
 const welcome = (number, groupname) => {
-    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 ${groupname}`
+    return `*_🌚Hola🍭 @${number} Te damos la Bienvenida a ${groupname} Espero y sea de su agrado🧘🏻‍♂️ ,contamos con su apoyo🔥 lee la descripción para evitar ser eliminado (🐤DIVIÉRTETE✨)_*`
 }
 exports.welcome = welcome
 
 const bye = (number) => {
-    return `Adeus @${number}. Já vai tarde 👋`
+    return `_😹Finalmente la carga del grupo se redujo en -1 *''Adiós''* @${number} No te extrañaremos😆, el próximo en salir será kuakiado ok.no_`
 }
 exports.bye = bye
